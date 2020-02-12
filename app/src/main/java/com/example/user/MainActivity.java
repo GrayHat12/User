9801bc2e402efbc2fcce4e7419dc54bb7fe5d3cc
GrayHat12 <grayhathacks10@gmail.com>
2020-02-12 20:19:01 +0530

    Firebase Integration

--- app/src/main/java/com/example/user/MainActivity.java
+++ app/src/main/java/com/example/user/MainActivity.java
@@ -1,15 +1,62 @@
 package com.example.user;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText data, amount;
+    EditText amount;
+    AutoCompleteTextView data;
     Button vendor, submit;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    List<Map<String,String>> items=null;
+    ArrayAdapter<String> adapter;
+
+    void initFirebaseItems()
+    {
+        database.getReference("ritik/items").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                items.clear();
+                adapter.clear();
+                for(DataSnapshot item : dataSnapshot.getChildren())
+                {
+                    Map<String,String> itm = (Map<String,String>)item.getValue();
+                    items.add(itm);
+                    System.out.println(itm);
+                    adapter.add(itm.get("name"));
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println(databaseError.getDetails());
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +66,45 @@ public class MainActivity extends AppCompatActivity {
         amount= findViewById(R.id.amount);
         vendor = findViewById(R.id.vendor);
         submit=findViewById(R.id.submit);
+        items = new LinkedList<>();
+        adapter = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, new LinkedList<String>());
+        data.setThreshold(1);
+        data.setAdapter(adapter);
+        initFirebaseItems();
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickedSubmit();
+            }
+        });
+        vendor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clcikedVendor();
+            }
+        });
+    }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    private void clcikedVendor() {
+        Intent vndr = new Intent(getApplicationContext(),Vendors.class);
+        startActivity(vndr);
+    }
+
+    private void clickedSubmit() {
     }
 }
--- app/src/main/java/com/example/user/Vendors.java
+++ app/src/main/java/com/example/user/Vendors.java
@@ -1,14 +1,80 @@
 package com.example.user;
 
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class Vendors extends AppCompatActivity {
+
+    ListView vendorList;
+    ArrayAdapter<String> adapter;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    List<Map<String,String>> vendors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendors);
+        vendorList = findViewById(R.id.recyclerView);
+        vendors = new LinkedList<>();
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, new LinkedList<String>());
+        vendorList.setAdapter(adapter);
+        initFirebaseVendors();
     }
+
+    private void initFirebaseVendors() {
+        database.getReference("ritik/vendors").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                vendors.clear();
+                adapter.clear();
+                for(DataSnapshot vendor : dataSnapshot.getChildren())
+                {
+                    Map<String,String> vndr = (Map<String,String>)vendor.getValue();
+                    vendors.add(vndr);
+                    adapter.add(vndr.get("name"));
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println(databaseError.getDetails());
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
 }
